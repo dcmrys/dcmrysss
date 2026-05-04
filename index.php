610fda8ad27237754cb39cb285366db7ce8e3a60
Maryclaire Jashley <dcmrys@gmail.com>
2026-05-04 10:56:05 +0800

    change tfa2 title

--- index.php
+++ index.php
@@ -4,4 +4,4 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-<title>Dcmrys TFA2</title>
+<title> TFA2</title>

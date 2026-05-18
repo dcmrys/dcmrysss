d2d27b1471fc56d3a739481d60d924462d10244c
dcmrys <dcmrys@gmail.com>
2026-05-18 21:53:02 +0800

    final

---
+++ TFA4/story3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Story 3</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php include('header.php'); ?>
+
+<div class="story-box">
+    <img src="mark.jpg">
+    <h2>Mark Benedict</h2>
+    <p>A young man named Mark was born on March 15th. He is known for being friendly and hardworking. During his free time, he enjoys playing online games and listening to music.</p>
+    <p>Mark is currently studying Information Technology at FEU Institute of Technology. He is a 2nd year student who dreams of becoming a successful web developer in the future.</p>
+</div>
+
+<?php require('footer.php'); ?>
+
+</body>
+</html>
